3babee8c898cc8fe37ad1047cce605bf38b0ef75
Dmitry Lebedev <dl@adios.ru>
2024-03-09 10:43:41 +0500

    composer.json changed.

--- autoload.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-/**
- * (c) 2005-2023 Dmitry Lebedev <dl@adios.ru>
- * This source code is the Ultra autoloader.
- */
-require_once \dirname(__DIR__).'/core/boot.php';
-\ultra\Boot::start(initial_path: \dirname(__DIR__, 2));
